c3e2eba346469a9c7ca072a1b3b517278d555b04
Gaja Jothi <gajalakshmi.a@gmail.com>
2023-01-04 23:50:18 +0530

    commited after useEffect,axios

--- reactamazon/reactamazon/src/components/Content/Main.js
+++ reactamazon/reactamazon/src/components/Content/Main.js
@@ -1,35 +1,46 @@
 import React, { useState, useEffect } from "react";
 import "./Main.css";
+import axios from "axios";
 
+//whenever this compnent render on dom, api should get called once;
+//whatever data is provided by api, we will store in state
+//we will manuipulate the data to show them on frontend
 export default function Main() {
   let [item, setItem] = useState([]);
   useEffect(() => {
     console.log("Inside useEffect");
     const fetchApi = async () => {
-      let response = await fetch("https://dummyjson.com/products");
-      let data = await response.json();
-      console.log(data);
-      let products = data.products;
-      setItem(products);
+      try {
+        let response = await axios.get("https://dummyjson.com/products");
+        // let data = await response.json();
+        // console.log(data);
+        let products = response.data.products;
+        // setItem((prev) => [...prev, products]);
+        setItem(products);
+      } catch (error) {
+        console.log(error);
+      }
     };
     fetchApi();
   }, []);
 
   return (
     <div className="container">
-      {item.map((ele) => (
-        <div className="card">
-          <div className="image-card">
-            <img src={ele.thumbnail} alt="" />
+      {/* {item.length !== 0 ? ( */}
+      {item.length &&
+        item.map((ele) => (
+          <div className="card">
+            <div className="image-card">
+              <img src={ele.thumbnail} alt="ele.title" />
+            </div>
+            <div className="card-content">
+              <p>{ele.title}</p>
+              <p>{ele.price}</p>
+              <p>{ele.rating}</p>
+              <button>Add To cart</button>
+            </div>
           </div>
-          <div className="card-content">
-            <p>{ele.title}</p>
-            <p>{ele.price}</p>
-            <p>{ele.rating}</p>
-            <button>Add To cart</button>
-          </div>
-        </div>
-      ))}
+        ))}
     </div>
 
     //  <div class="card">
--- reactamazon/reactamazon/src/components/Subheader/Subheader.js
+++ reactamazon/reactamazon/src/components/Subheader/Subheader.js
@@ -1,20 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./subheader.css";
 
 export default function Subheader() {
+  //making state
+  let [hover, setHover] = useState(false);
+
+  //inline style attribute takes an object
+  let mystyles = {
+    backgroundColor: "grey",
+  };
+
+  let hoverStyle = {
+    border: hover ? "1px solid black" : "none",
+  };
+
+  const handleHoverIn = () => {
+    setHover(true);
+  };
+  const handleHoverOut = () => {
+    setHover(false);
+  };
+
+  useEffect(() => {
+    let subheader = document.querySelector(".subheader");
+    console.log(subheader.children);
+    let childofsubheader = subheader.children;
+    for (let i = 0; i < childofsubheader.length; i++)
+      childofsubheader[i].addEventListener("mouseenter", () => {
+        childofsubheader[i].style.border = "1px solid black";
+      });
+    for (let i = 0; i < childofsubheader.length; i++)
+      childofsubheader[i].addEventListener("mouseleave", () => {
+        childofsubheader[i].style.border = "none";
+      });
+  });
+
   return (
-    <div className="subheader">
-      <div>
+    <div className="subheader" style={mystyles}>
+      <div
+      // onMouseEnter={handleHoverIn}
+      // onMouseLeave={handleHoverOut}
+      // style={hoverStyle}
+      >
         <span>
           <i className="fa fa-reorder"></i>
           All
         </span>
       </div>
-      <div>Today's Deals</div>
-      <div>Customer Serivce</div>
-      <div>Registry</div>
-      <div>GIft Cards</div>
-      <div>Sell</div>
+      <div
+      // onMouseEnter={handleHoverIn}
+      // onMouseLeave={handleHoverOut}
+      // style={hoverStyle}
+      >
+        Today's Deals
+      </div>
+      <div
+      // onMouseEnter={handleHoverIn}
+      // onMouseLeave={handleHoverOut}
+      // style={hoverStyle}
+      >
+        Customer Serivce
+      </div>
+      <div
+      // onMouseEnter={handleHoverIn}
+      // onMouseLeave={handleHoverOut}
+      // style={hoverStyle}
+      >
+        Registry
+      </div>
+      <div
+      // onMouseEnter={handleHoverIn}
+      // onMouseLeave={handleHoverOut}
+      // style={hoverStyle}
+      >
+        GIft Cards
+      </div>
+      <div
+      // onMouseEnter={handleHoverIn}
+      // onMouseLeave={handleHoverOut}
+      // style={hoverStyle}
+      >
+        Sell
+      </div>
     </div>
   );
 }
