73458fb2060baa2d924b41edcd80c0021d974ef1
Gaja Jothi <gajalakshmi.a@gmail.com>
2023-01-09 15:19:44 +0530

    RubyonRailsFirst

---
+++ RubyonRails/easy-blog/app/controllers/register_controller.rb
@@ -0,0 +1,5 @@
+class RegisterController < ApplicationController
+    def login
+    
+    end
+end
---
+++ RubyonRails/easy-blog/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :articles
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # root "application#hello"
+  root "pages#home"
+  get "aboutus", to: "pages#about"
+  get "register", to: "register#login"
+end
